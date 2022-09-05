5401549d24795fea85fddf8a25a410a34b4054a9
Natahis <natahis@bk.ru>
2022-09-05 19:17:32 +0300

    Task60

---
+++ Task60/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, 
+// добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+int[,,] GreateMatrixRndInt(int row, int col, int err, int min, int max)
+{
+    int[,,] matrix = new int[row, col, err];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int z = 0; z < matrix.GetLength(2); z++)
+            {
+                matrix[i, j, z] = rnd.Next(min, max + 1);
+            }
+
+        }
+    }
+    return matrix;
+}
+
+
+void PrintMatrix(int[,,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int z = 0; z < matrix.GetLength(2); z++)
+            {
+                Console.Write($"{matrix[i, j, z],4}({i},{j},{z})");
+            }
+            Console.WriteLine();
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,,] arr3D = GreateMatrixRndInt(2, 2, 2, 10, 100);
+PrintMatrix(arr3D);
