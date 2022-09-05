9567c09984a59d8ebbc384498d167a62b10d6637
Natahis <natahis@bk.ru>
2022-09-05 16:12:28 +0300

    Task54

---
+++ Task54/Program.cs
@@ -0,0 +1,83 @@
+﻿// Задача 54: Задайте двумерный массив. 
+// Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int[,] GreateMatrixRndInt(int row, int col, int min, int max)
+{
+    int[,] matrix = new int[row, col];
+    Random rnd = new Random();
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            // if (j < matrix.GetLength(1) - 1) Console.Write($"{matrix[i, j], 4},");
+            // else Console.Write($"{matrix[i, j], 4}");
+            Console.Write(j < matrix.GetLength(1) - 1 ? $"{matrix[i, j],3}," : $"{matrix[i, j],3}");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+int[,] arr2D = GreateMatrixRndInt(4, 5, 0, 9);
+PrintMatrix(arr2D);
+Console.WriteLine();
+
+// int[,] NewMatrix(int[,] matrix)
+// {
+//     int[,] newmatrix = new int[matrix.GetLength(0),matrix.GetLength(1)];
+//     int max = 0;
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+//             if (matrix[i, j] > max) max = matrix[i,j];
+
+//         }
+//     newmatrix[i,j] = matrix[i,j];   
+//     }
+// }
+// return newmatrix;
+
+// int[,] newarr2D = NewMatrix(arr2D);
+
+void NewMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int z = 0; z < matrix.GetLength(1) - 1; z++)
+            {
+                if (matrix[i, z] < matrix[i, z + 1])
+                {
+                    int temp = matrix[i, z + 1];
+                    matrix[i, z + 1] = matrix[i, z];
+                    matrix[i, z] = temp;
+                }
+            }
+        }
+    }
+
+}
+NewMatrix(arr2D);
+PrintMatrix(arr2D);
